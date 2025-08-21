09b7e0ea6985addcfd0131b05d3f3f3376e136bc
Kiran Jojare <kiranjojare@Kirans-MacBook-Pro.local>
2025-08-21 18:17:27 -0400

    first program

---
+++ Module2_Getting Started/01_hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/* 
+A namespace is a region that defines the context in 
+which variables and functions can be used. All the elements 
+of the C++ standard library are declared within a region called 
+an std.
+*/
+using namespace std;
+
+int main(){
+    cout << "Hello World!";
+    return 0;
+}
