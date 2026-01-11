424167a11f182b418aeb9224d659ca7026a8be37
Kiran Jojare <111842372+kiranj26@users.noreply.github.com>
2026-01-10 21:10:28 -0500

    More laernings

--- Module2_OOPS/01_Introduction/02_classes.cpp
+++ Module2_OOPS/01_Introduction/02_classes.cpp
@@ -110,9 +110,116 @@ class Student{
             cout << "Age: " << age << ", Height: " << height << ", Weight: " << weight << "\n";
         }
 };
+
+// Class Data Members Initialization
+// We have initialised variables data members to a default in the class
+// Please note we cant define array members with default values in the class
+class Circle{
+    private:
+        int radius = 1; // data member initialized to 1
+        string mod = "default"; // data member initialized to default
+        // int arr[] = {1,2,3,4,5}; // error: array data members cannot be initialized like this
+        /////////////////////
+        // IMPORTANT NOTE: //
+        /////////////////////
+        // NOTEEEEE : This above could work for local variables but not for the class data members
+        // For embedded arrays use like this
+        // std::array<int, 5> arr = {1,2,3,4,5};
+        //////////////
+
+        // uncommenting this will cause compilation error
+        // becuase array data members cannot be initialized like this
+        // if we declare array data members without providing size it will also cause error
+    public:
+        void setRadius(int r){
+            radius = r;
+        }
+        int getRadius(){
+            return radius;
+        }
+        float getArea(){
+            return 3.14 * radius * radius;
+        }
+};
+
+
+// Class Member Functions
+// Functions defined inside the class are called member functions
+// They are used to access and manipulate the data members of the class
+// Mostly public member functions are used to provide interface to access private data members
+// However member functions can also be private
+
+class ractangle{
+    int length;
+    int breadth;
+
+    public: 
+        void getArea(){
+            cout << "Area of rectangle is: " << length * breadth << "\n";
+        }
+        void setData(int l, int b){
+            length = l;
+            breadth = b;
+        }
+};
+/////////////////////
+// IMPORTANT NOTE: //
+/////////////////////
+// Scope resolution operator (::)
+// Member functions can also be defined outside the class using scope resolution operator (::)
+/// Example of using scope resolution operator on a rectangle class
+
+class newrectangle{
+    int length;
+    int breadth;
+
+    public:
+        void setData(int l, int b);
+        int getArea();
+
+        void setLength(int l);
+        void setBreadth(int b);
+
+        int getLength();
+        int getBreadth();
+};
+
+// format is return_type class_name :: function_name(parameters)
+void newrectangle::setData(int l, int b){
+    length = l;
+    breadth = b;
+}
+int newrectangle::getArea(){
+    return length * breadth;
+}
+
+
+// set and get functions for the data members using scope operator
+void newrectangle::setLength(int l){
+    length = l;
+}
+void newrectangle::setBreadth(int b){
+    breadth = b;
+}
+
+int newrectangle::getLength(){
+    return length;
+}
+int newrectangle::getBreadth(){
+    return breadth;
+}
+
+/////////////////////
+// IMPORTANT NOTE: //
+/////////////////////
+// OVERLOADING MEMBER FUNCTIONS
+//// Member functions can be overloaded just like any other function. 
+// This means that multiple member functions can exist with the same name on 
+// the same scope, but must have different arguments.
+
 int main(void){
     // object of a class similar to a variable
-    className obj1; // object of class className
+    newrectangle obj1; // object of class newrectangle
 
     // Accesing the class members using object and public functions
     JojareFamily family1;
@@ -121,8 +228,14 @@ int main(void){
     // accesing private memebers directly will cause error
     // family1.total_members = 10; // error: 'total_members' is a private member of 'JojareFamily'
     
-    
+    // Access datta member initilisation of class Circle
+    Circle c1;
 
+    cout << "Area of a circle with default radius " << c1.getRadius() << " is ::" << c1.getArea() << endl;
+    // set radius ot a different value 
+    c1.setRadius(5);
+
+    cout << "Area of a circle with new radius " << c1.getRadius() << " is ::" << c1.getArea() << endl;
     return 0;
 }
 
